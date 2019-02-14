d2eecb99d602785d243bfe5c9069c24046afa443
Bowen <z.bowen66@gmail.com>
2019-02-14 11:28:21 +0800

    add antd

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
-<style lang="scss">
-#app {
-  width: max-content;
-  margin: 0 auto;
-  input {
-    width: -webkit-fill-available;
-  }
-}
-</style>
-
-<style src="./app.less"></style>
-
-
 <template>
   <div id="app">
-    <input v-model="msg">
-    <p>{{ msg }}</p>
+    <a-form
+      :form="form"
+      @submit="onSubmit"
+    >
+      <a-form-item label="反馈意见">
+        <a-textarea
+          v-decorator="['content', {
+            rules: [{
+              required: true,
+              min: 20,
+              message: '请填写20字以上',
+            }],
+            validateTrigger: 'blur'
+          }]"
+          :autosize="{ minRows: 6 }"
+          placeholder="请填写15字以上的意见或建议，以便我们提供更好的服务"
+        ></a-textarea>
+      </a-form-item>
+      <a-form-item>
+        <a-button
+          html-type="submit"
+          type="primary"
+        >提交</a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
@@ -22,8 +32,15 @@
 export default {
   data() {
     return {
-      msg: 'just ssr',
+      form: this.$form.createForm(this),
     };
+  },
+  methods: {
+    async onSubmit(e) {
+      e.preventDefault();
+      const values = await this.form.validateFields();
+      console.log(values);
+    },
   },
 };
 </script>
--- src/entry/main.js
+++ src/entry/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue';
 import App from '../App';
+
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(Antd);
 
 export default context => ({
   app: new Vue({
